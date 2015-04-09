b9adc82022e6d1bf81486a93e47fd442fd0e0f40
Rendol <sapegin.in@gmail.com>
2015-04-09 19:26:43 +0600

    test page + add function IS.extend

--- src/InfinitySystems.js
+++ src/InfinitySystems.js
@@ -7,6 +7,14 @@
 var InfinitySystems = {
 	prototypes: {},
 	classes: {},
+	creater: function(prototype) {
+		if ('Class' in window) {
+			return Class.apply(arguments);
+		}
+		return function () {
+			return prototype;
+		};
+	},
 	reg: function (name, prototype) {
 		this.prototypes[name] = prototype;
 	},
@@ -18,8 +26,8 @@ var InfinitySystems = {
 	},
 	cls: function (name, prototype) {
 		if (!(name in this.classes)) {
-			this.classes[name] = Class($.extend(
-				true,
+			this.classes[name] = this.creater(IS.extend(
+				{},
 				this.get(name),
 				prototype
 			));
@@ -28,6 +36,25 @@ var InfinitySystems = {
 	},
 	crt: function (name, config, prototype) {
 		return new (this.cls(name, prototype))(config);
+	},
+	extend: function (obj) {
+		Array.prototype.slice.call(arguments, 1).forEach(function (source) {
+			if (source) {
+				for (var prop in source) {
+					if (source[prop].constructor === Object) {
+						if (!obj[prop] || obj[prop].constructor === Object) {
+							obj[prop] = obj[prop] || {};
+							IS.extend(obj[prop], source[prop]);
+						} else {
+							obj[prop] = source[prop];
+						}
+					} else {
+						obj[prop] = source[prop];
+					}
+				}
+			}
+		});
+		return obj;
 	}
 };
 var IS = InfinitySystems;
